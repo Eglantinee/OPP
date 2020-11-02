ae1074e87fbf05ab892d6e4928d1630e27d6fd24
Eglantinee <twostepsfromoblivion@gmail.com>
2020-11-02 20:01:27 +0200

    initial and maybe final commit if all will be ok

---
+++ lab4/data.c
@@ -0,0 +1,183 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+// voids and functions to deal with Matrixes
+void mx_product(int **mx1, int **mx2, int **res_mx, int N){
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			res_mx[i][j] = 0;
+			for (int k = 0; k < N; k++){
+				res_mx[i][j] += mx1[i][k] * mx2[k][j];
+			}
+		}
+	}
+}
+void mx_sum(int **mx1, int **mx2, int **res_mx, int N){
+	#pragma omp parallel for collapse(2)
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			res_mx[i][j] = mx1[i][j] + mx2[i][j];
+		}
+	}
+}
+int mx_min(int **mx, int N){
+	int res = mx[0][0];
+	for (int i = 1; i < N; i++)
+	for (int j = 1; j < N; j++)
+	if (mx[i][j] < res){
+		res = mx[i][j];
+	}
+	return res;
+}
+int mx_max(int **mx, int N){
+	int res = mx[0][0];
+	for (int i = 1; i < N; i++)
+	for (int j = 1; j < N; j++)
+	if (mx[i][j] > res){
+		res = mx[i][j];
+	}
+	return res;
+}
+void fill_mx(int **mx, int N, int num){
+	#pragma omp parallel for collapse(2)
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			mx[i][j] = num;
+		}
+	}
+}
+// Voids and functions to deal with Vectors
+void vect_sum(int v1[], int v2[], int res_vect[], int N){
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++){
+		res_vect[i] = v1[i] + v2[i];
+	}
+}
+int vect_max(int v1[], int N){
+	int res = v1[0];
+	for (int i = 0; i < N; i++){
+		if (v1[i] > res){
+			res = v1[i];
+		}
+	}
+	return res;
+}
+int vect_min(int v1[], int N){
+	int res = v1[0];
+	for (int i = 0; i < N; i++){
+		if (v1[i] < res){
+			res = v1[i];
+		}
+	}
+	return res;
+}
+void fill_vect(int v1[], int N, int num){
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++){
+		v1[i] = num;
+	}
+}
+// Void to deal with product of Matrix and Vector
+void product(int **mx, int v1[], int res_vect[], int N){
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++){
+		res_vect[i] = 0;
+		for (int j = 0; j < N; j++){
+			res_vect[i] += mx[i][j] * v1[j];
+		}
+	}
+}
+//Free memory which is used by matrixes
+void clear_mx(int **mx, int N){
+	#pragma omp parallel for
+	for (int i = 0; i < N; ++i){
+		free(mx[i]);
+	}
+	free(mx);
+}
+// Functions F1, F2, F3
+int F1( int **mx1,
+		int **mx2,
+		int A[],
+		int B[],
+		int C[],
+		int N
+){
+	int res;
+	int tmp_vect[N];
+	int tmp_vect2[N];
+	int **tmp_mx = (int **)malloc(N * sizeof(int *));
+	for (int i = 0; i < N; ++i){
+		tmp_mx[i] = (int *)malloc(N * sizeof(int));
+	}
+	vect_sum(B, C, tmp_vect, N);
+	res = vect_max(tmp_vect, N);
+	mx_product(mx1, mx2, tmp_mx, N);
+	product(tmp_mx, B, tmp_vect, N);
+	vect_sum(tmp_vect, A, tmp_vect2, N);
+	res += vect_min(tmp_vect2, N);
+	clear_mx(tmp_mx, N);
+	clear_mx(mx1, N);
+	clear_mx(mx2, N);
+	return res;
+}
+int F2( int **mx1,
+		int **mx2,
+		int **mx3,
+		int **mx4,
+		int N
+){
+	int res;
+	int **tmp_mx1 = (int **)malloc(N * sizeof(int *));
+	int **tmp_mx2 = (int **)malloc(N * sizeof(int *));
+	for (int i = 0; i < N; ++i){
+		tmp_mx1[i] = (int *)malloc(N * sizeof(int));
+		tmp_mx2[i] = (int *)malloc(N * sizeof(int));
+	}
+	mx_product(mx1, mx2, tmp_mx1, N);
+	mx_sum(mx3, mx4, tmp_mx2, N);
+	res = mx_max(tmp_mx1, N);
+	res -= mx_min(tmp_mx2, N);
+	clear_mx(tmp_mx1, N);
+	clear_mx(tmp_mx2, N);
+	clear_mx(mx1, N);
+	clear_mx(mx2, N);
+	clear_mx(mx3, N);
+	clear_mx(mx4, N);
+	return res;
+}
+int *F3(int **mx1,
+		int **mx2,
+		int v1[],
+		int v2[],
+		int N
+){
+	int *res_vect;
+	int **tmp_mx = (int **)malloc(N * sizeof(int *));
+	for (int i = 0; i < N; ++i){
+		tmp_mx[i] = (int *)malloc(N * sizeof(int));
+	}
+	res_vect = malloc(N * sizeof(int));
+	if (res_vect == NULL){return NULL;}
+	mx_product(mx1, mx2, tmp_mx, N);
+	product(tmp_mx, v1, res_vect, N);
+	vect_sum(res_vect, v2, res_vect, N);
+	clear_mx(tmp_mx, N);
+	clear_mx(mx1, N);
+	clear_mx(mx2, N);
+	return res_vect;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lab4/main.c
@@ -0,0 +1,115 @@
+//----------------------------------------------------------------
+//        --  Лабортаторна робота №4 Варіант 27
+//        --  F1: d = MAX(B + C) + MIN(A + (MA * ME)*B)
+//        --  F2: f = MAX(MG*MK) - MIN(ML + MН)
+//        --  F3: O = (MP*MR)*S + V
+//        --  Шкардибарда І. ІВ-82
+//        --  Дата 14.10.2020
+//----------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "data.h"
+void t1(int N);
+void t2(int N);
+void t3(int N);	
+int main(int argc, char **argv){
+	// GOMP_CPU_AFFINITY="1 2 3" ./main -> bind thread to CPU
+	// OMP_STACKSIZE=10M ./main -> set new stecksize
+	// sleep() -> sleep for some time
+	int N = 1000;
+	omp_set_nested(1);
+	//t1(N);
+	//t2(N);
+	//t3(N);
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		{
+			t1(N);
+		}
+		#pragma omp section
+		{
+			t2(N);
+		}
+		#pragma omp section
+		{
+			t3(N);
+		}
+	}	
+	return 0;
+}
+void t1(int N){
+	int **MA = (int **)malloc(N * sizeof(int *));
+	int **ME = (int **)malloc(N * sizeof(int *));
+	int A[N];
+	int B[N];
+	int C[N];
+	int d;
+	for (int i = 0; i < N; ++i){
+		MA[i] = malloc(N * sizeof(int));
+		ME[i] = malloc(N * sizeof(int));
+	}
+	printf("T1 is started:\n");
+	fill_mx(MA, N, 1);
+	fill_mx(ME, N, 1);
+	fill_vect(A, N, 1);
+	fill_vect(B, N, 1);
+	fill_vect(C, N, 1);
+	d = F1(MA, ME, A, B, C, N);
+	#pragma omp critical
+	{
+		printf("Result of F1 is %d\n", d);
+		printf("T1 is finished\n");
+	}
+}
+void t2(int N){
+	int **MG = (int **)malloc(N * sizeof(int *));
+	int **MK = (int **)malloc(N * sizeof(int *));
+	int **ML = (int **)malloc(N * sizeof(int *));
+	int **MH = (int **)malloc(N * sizeof(int *));
+	int f;
+	for (int i = 0; i < N; ++i){
+		MG[i] = malloc(N * sizeof(int));
+		MK[i] = malloc(N * sizeof(int));
+		ML[i] = malloc(N * sizeof(int));
+		MH[i] = malloc(N * sizeof(int));
+	}
+	printf("T2 is started:\n");
+	fill_mx(MG, N, 2);
+	fill_mx(ML, N, 2);
+	fill_mx(MK, N, 2);
+	fill_mx(MH, N, 2);
+	f = F2(MG, MK, ML, MH, N);
+	#pragma omp critical 
+	{
+		printf("Result of F2 is %d\n", f);
+		printf("T2 is finished\n");
+	}
+}
+void t3(int N){
+	int **MP = (int **)malloc(N * sizeof(int *));
+	int **MR = (int **)malloc(N * sizeof(int *));
+	int S[N];
+	int V[N];
+	int *O;
+	for (int i = 0; i < N; ++i){
+		MP[i] = malloc(N * sizeof(int));
+		MR[i] = malloc(N * sizeof(int));
+	}
+	printf("T3 is started:\n");
+	fill_mx(MP, N, 3);
+	fill_mx(MR, N, 3);
+	fill_vect(S, N, 3);
+	fill_vect(V, N, 3);
+	O = F3(MP, MR, S, V, N);
+	#pragma omp critical
+	{
+		printf("Result of F3 is vect: ");
+		for (int i = 0; i < N; i++){
+			printf("%d ", O[i]);
+		}
+		printf("\nT3 is finished\n");
+	}
+	free(O);	
+}
